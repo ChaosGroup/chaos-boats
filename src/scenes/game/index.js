dbff1b28633c129256acc81ded92a6599b2deaa9
Martin Dimitrov <martin@sachmata.com>
2020-11-15 16:27:52 +0200

    Use node 14 for build

--- src/scenes/game/index.js
+++ src/scenes/game/index.js
@@ -22,7 +22,7 @@ const GAME_ROUNDS = HEADLESS_RATING ? 6 : 3;
 const GAME_TIMER = HEADLESS_RATING ? 1 * 6e4 : 3 * 6e4; // min
 
 const PLAYERS_TURN_STEP = 300;
-const PLAYERS_TURN_TIMEOUT = 100;
+const PLAYERS_TURN_TIMEOUT = 200;
 
 const BASE_TEXT_STYLE = {
 	fontFamily: 'Eczar, serif',
